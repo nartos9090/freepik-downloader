e1cb5a57e9b31523db5da6fbd5a02525a0053a27
Widya Ardianto <nartos9090@gmail.com>
2022-08-30 16:44:19 +0700

    lib index

--- index.ts
+++
@@ -1,7 +0,0 @@
-const auth = require('./src/auth')
-const downloader = require('./src/downloader')
-
-module.exports = {
-  auth,
-  downloader
-}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const auth = require('./auth')
+const downloader = require('./downloader')
+
+module.exports = {
+  auth,
+  downloader
+}
