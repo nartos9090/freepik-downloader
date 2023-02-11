cd3a9a4f4f5306abe22d8ae9abed77b95948d528
Widya Ardianto <nartos9090@gmail.com>
2023-02-11 16:48:47 +0700

    create queue mongoose model

---
+++ src/api/model.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+import {MONGO_URI} from './config'
+
+mongoose.connect(MONGO_URI).then(r => console.log('Connected to MongoDB'))
+    .catch(e => console.log('Error connecting to MongoDB'))
+
+export const Queue = mongoose.model('Queue', new mongoose.Schema({
+        webhook_url: {
+            type: String,
+            required: true
+        },
+        download_url: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['queued', 'downloading', 'completed', 'failed'],
+            default: 'queued'
+        }
+    }, {
+        versionKey: false,
+        toJSON: {
+            virtuals: true,
+            versionKey: false,
+            transform: function (doc, ret) {
+                delete ret._id
+            }
+        }
+    }))
+
+;(async () => {
+    await Queue.updateMany({status: 'downloading'}, {$set: {status: 'queued'}})
+})()
