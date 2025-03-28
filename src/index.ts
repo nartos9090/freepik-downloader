ac5c317fbe55ba186b54493e58d24f3965c9539e
Widya Ardianto <nartos9090@gmail.com>
2025-03-28 18:32:21 +0700

    add export getSavedCookie

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { setCookie } from './cookie/cookie'
+import { getSavedCookie, setCookie } from './cookie/cookie'
 import { downloadByUrl as downloadByUrlV2 } from './downloader/auto-downloader'
 
 export default {
   setCookie,
+  getSavedCookie,
   downloadByUrlV2
 }
